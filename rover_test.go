4e801444d87f5e403c016e94ad6095caf7bfbe5d
Josh Hill <jamesjoshuahill@gmail.com>
2017-11-19 13:38:41 +0000

    Dry out creation of rover in tests

--- rover_test.go
+++ rover_test.go
@@ -8,25 +8,27 @@ import (
 )
 
 var _ = Describe("Rover", func() {
+	var rover pluto.Rover
+
+	BeforeEach(func() {
+		rover = pluto.NewRover()
+	})
+
 	It("starts at position 0,0 heading north", func() {
-		rover := pluto.NewRover()
 		Expect(rover.Position()).To(Equal(pluto.Position{X: 0, Y: 0, Heading: pluto.NORTH}))
 	})
 
 	It("can do one command", func() {
-		rover := pluto.NewRover()
 		rover.Do("F")
 		Expect(rover.Position()).To(Equal(pluto.Position{X: 0, Y: 1, Heading: pluto.NORTH}))
 	})
 
 	It("can do all four commands", func() {
-		rover := pluto.NewRover()
 		rover.Do("FLBR")
 		Expect(rover.Position()).To(Equal(pluto.Position{X: 1, Y: 1, Heading: pluto.NORTH}))
 	})
 
 	It("can do many commands", func() {
-		rover := pluto.NewRover()
 		rover.Do("FFRFF")
 		Expect(rover.Position()).To(Equal(pluto.Position{X: 2, Y: 2, Heading: pluto.EAST}))
 	})
